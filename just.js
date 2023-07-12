0967b1f48e26bb30377ce2fb9d38356004a56ad9
Muslimakhon Shokirova <shokirovamuslimakhon@gmail.com>
2023-07-13 00:30:30 +0500

    somethiiiing

---
+++ just.js
@@ -0,0 +1,29 @@
+// Bismillah
+
+// Execution Context - deb sizning kodlaringizni o'qishi va ishga tushirish jarayoniga aytiladi
+
+// 1. Creation Phase
+   // 1.1 creates global object
+   // 1.2 this: global object ( this'ni global objectga tenglashtirib qo'yadi) (global obj in Node Js)(window obj in Browser)
+   // 1.3RAM(heap memory)   your variables and functions are stored in heap memory aaand her values will equal to undefined , functionlarni esa body'siga tenglashtirib qoyadi undefined ga emas
+   
+
+
+// Decl boration bo'lgan variable'lar global/window obj ni ichida bo'lmaydi. Let , cons , var da e'lon qilingan bo'lsa declare bo'lgan hisolanadi. Example:
+   let name = "Kimsan";
+
+    // asing bo'lgan xolati esa
+     ism = "Alexandra";
+     
+     console.log(global);
+
+
+// 2.Execution Phase
+   //2.1  your variables initalized aand functions invoked(called) 
+   //2.2  
+
+
+
+   //--------------------------------------------------------------------------------------------------
+
+   // Weakly type - values'sini data type'ini qanaqa ekanligini aytmasangiz ham o'zi bilib oladi
